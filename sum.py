ae7c097ce4a0846e38a90e69ce140807e639106b
keerthisanaboda <keerthisanaboda@gmail.com>
2026-07-24 20:50:14 +0530

    sum.py
    This program moves beyond static text to capture user entries and perform mathematical operations.

---
+++ sum.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+# Calculate sum
+total = num1 + num2
+print(f"The sum of {num1} and {num2} is: {total}")
+#output:
+#Enter first number: 12.5
+#Enter second number: 7
+#The sum of 12.5 and 7 is 19.5
